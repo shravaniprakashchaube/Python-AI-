6ccd289cf72eab37b72e87509571d3cb5b47595a
shravani <92591486+shravaniprakashchaube@users.noreply.github.com>
2026-02-02 16:58:31 +0530

    Add initial Python script with basic examples
    Initial implementation of Python script demonstrating basic syntax, variable types, and comments.

---
+++ Python1.py
@@ -0,0 +1,30 @@
+# Python is simple and easy to understand
+# Python is an open source platform
+print('Hello world')
+print(987982749234)
+# syntax to store the value in a variable/identifier
+place = 'MUMBAI'
+len(place)
+# Language is composed of = Syntax and Symantics
+# Syntax: how language is structured
+# Semantic: How we can use its functionality
+# Data type is used to represent a data in a particular format
+# Numeric: int, float,complex
+a = 100
+# check type
+#  while writing a code we have 2 options
+# 1. Code itself
+# 2. Comment: when dev. wants to  provide additional information about the code then he/she can use comment
+# hash in prefix is a comment
+type(a)
+type(place)
+b = 44.33
+type(b)
+# check the type of data
+c = 4+5j   # complex
+d = [1,2.,'3'] # list
+e = (1,2,3) # tuple
+f = {1,2,3} # set
+g = {'a':1,'b':2} # dict
+d
+type(a),type(b), type(c),type(d),type(e),type(f),type(g)
