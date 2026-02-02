793cc9fe57577a5da2c7020de3e527cba70dc246
shravani <92591486+shravaniprakashchaube@users.noreply.github.com>
2026-02-02 17:13:14 +0530

    Add Python2.py with basic Python concepts

---
+++ Python2.py
@@ -0,0 +1,27 @@
+# numeric: int, float,complex
+# str: " " ' '
+# List [ ]
+# Tuple ( )
+# Set {atleast 1 value}
+# dict {key:value}
+a = 'this is python'
+# int a = 100
+# string s = 'sample'
+# this is static typing
+# python is a dynamically typed language
+# python understands its data type at runtime, user need to add data type implicitly in a code
+a.upper()
+# to get the information of any method or any process valid in python
+# we can use help() function
+help(a.upper)
+help(a.count)
+# check total blocks present in str
+len(a)
+# Str supports indexing
+# indexing means a dedicated number allocated to each block of a string
+# to use index we have take [ ] square bracket
+#a[index]
+a[0]
+# we have +ve and -ve index as well
+# +ve indexing starts with Zero- Left to Right
+# -ve indexing starts with -1 - Right to Left
