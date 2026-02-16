968e20fd65e7710f17f8757e5449fdf8dd5ea190
shravani <92591486+shravaniprakashchaube@users.noreply.github.com>
2026-02-17 01:06:34 +0530

    Enhance set&amp;dict.py with set features and examples
    Added comments and examples demonstrating set features and operations.

---
+++ set&dict.py
@@ -0,0 +1,32 @@
+# Both are mutable data types
+# set- {at least one element inside}
+s = {}
+type(s)
+s1 = {1,}
+type(s1)
+# Features of set
+# duplicates are not allowed
+p = {1,2,3,4,1,1,1,3,3,3,4,4,2,2,2}
+p
+# Does'nt preserves sequence order
+q = {1,290,'3',4,1,1,'A',3,3,'B',4,4,'Python',2,2,0}
+q
+s={'Za11.5','Ba','Ab','C',2,2,'299',2,1,1,1,3,3,3,3,5,'5abc',5,4,4,4,9,9,9,7,8,8}
+s
+# Background data structure of a set is a Hash Table
+# Hash table doesnt support indexing and slicing
+# Set is Mutable
+s
+# try to delete members of a set
+s.pop()
+s.pop()
+winner = lot.pop()
+winner
+print('Original lot:',lot)
+out = lot.pop()
+print('Element removed:',out,'Current set:',lot)
+# remove(value)
+help(s.remove)
+# if we try to remove an element which is not present
+p.remove('java')
+# Assignment: Check all the methods of a Set
